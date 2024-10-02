b9ecc8b454dbf68dbb405f1deacb4ccd484d42fb
ubata-mamezou <masato-ubata@mamezou.com>
2024-10-02 10:51:54 +0000

    Deploying to gh-pages from @ ubata-mamezou/consider-ui@685a07afa51be152f407ac29fec2842445d9dc5d 🚀

---
+++ assets/Text.stories--2e7ZEth.js
@@ -0,0 +1,214 @@
+import{T as be}from"./Text-73_ukKtl.js";import"./assertThisInitialized-CGz1coAQ.js";import"./index-l2PZgWEW.js";import"./index-BtTJd_qj.js";const Fe={title:"components/Text",component:be,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{},args:{id:"demo",label:"テキスト表示名",onFocus:()=>{}}},e={parameters:{docs:{description:{story:"通常のテキスト(初期値、ヘルパーテキストも併せて設定しています)"}}},args:{defaultValue:"テキスト初期値",helperText:"テキストヘルパーテキスト"}},r={parameters:{docs:{description:{story:"必須入力テキスト"}}},args:{required:!0}},s={parameters:{docs:{description:{story:"読み取り専用テキスト"}}},args:{readOnly:!0}},a={parameters:{docs:{description:{story:"パスワード入力テキスト"}}},args:{type:"password"}},t={parameters:{docs:{description:{story:"数値入力テキスト"}}},args:{type:"number"}},n={parameters:{docs:{description:{story:"検索キーワード入力テキスト"}}},args:{type:"search"}},o={parameters:{docs:{description:{story:"カラーピッカー"}}},args:{type:"color",label:"",defaultValue:"チェックボックス"}},c={parameters:{docs:{description:{story:"日付入力テキスト"}}},args:{type:"date"}},p={parameters:{docs:{description:{story:"日時入力テキスト"}}},args:{type:"datetime-local"}},d={parameters:{docs:{description:{story:"メールアドレス入力テキスト"}}},args:{type:"email"}},i={parameters:{docs:{description:{story:"ファイル入力テキスト"}}},args:{type:"file"}},m={parameters:{docs:{description:{story:"非表示テキスト"}}},args:{type:"hidden",label:""}},u={parameters:{docs:{description:{story:"image"}}},args:{type:"image"}},y={parameters:{docs:{description:{story:"月入力テキスト"}}},args:{type:"month"}},l={parameters:{docs:{description:{story:"range"}}},args:{type:"range"}},g={parameters:{docs:{description:{story:"電話番号入力テキスト"}}},args:{type:"tel"}},T={parameters:{docs:{description:{story:"時間入力テキスト"}}},args:{type:"time"}},x={parameters:{docs:{description:{story:"URL入力テキスト"}}},args:{type:"url"}},S={parameters:{docs:{description:{story:"週入力テキスト"}}},args:{type:"week"}};var h,f,k;e.parameters={...e.parameters,docs:{...(h=e.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: '通常のテキスト(初期値、ヘルパーテキストも併せて設定しています)'
+      }
+    }
+  },
+  args: {
+    defaultValue: 'テキスト初期値',
+    helperText: 'テキストヘルパーテキスト'
+  }
+}`,...(k=(f=e.parameters)==null?void 0:f.docs)==null?void 0:k.source}}};var b,w,P;r.parameters={...r.parameters,docs:{...(b=r.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: '必須入力テキスト'
+      }
+    }
+  },
+  args: {
+    required: true
+  }
+}`,...(P=(w=r.parameters)==null?void 0:w.docs)==null?void 0:P.source}}};var q,V,F;s.parameters={...s.parameters,docs:{...(q=s.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: '読み取り専用テキスト'
+      }
+    }
+  },
+  args: {
+    readOnly: true
+  }
+}`,...(F=(V=s.parameters)==null?void 0:V.docs)==null?void 0:F.source}}};var O,L,M;a.parameters={...a.parameters,docs:{...(O=a.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: 'パスワード入力テキスト'
+      }
+    }
+  },
+  args: {
+    type: 'password'
+  }
+}`,...(M=(L=a.parameters)==null?void 0:L.docs)==null?void 0:M.source}}};var R,U,_;t.parameters={...t.parameters,docs:{...(R=t.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: '数値入力テキスト'
+      }
+    }
+  },
+  args: {
+    type: 'number'
+  }
+}`,...(_=(U=t.parameters)==null?void 0:U.docs)==null?void 0:_.source}}};var E,j,v;n.parameters={...n.parameters,docs:{...(E=n.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: '検索キーワード入力テキスト'
+      }
+    }
+  },
+  args: {
+    type: 'search'
+  }
+}`,...(v=(j=n.parameters)==null?void 0:j.docs)==null?void 0:v.source}}};var z,A,B;o.parameters={...o.parameters,docs:{...(z=o.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: 'カラーピッカー'
+      }
+    }
+  },
+  args: {
+    type: 'color',
+    label: '',
+    defaultValue: 'チェックボックス'
+  }
+}`,...(B=(A=o.parameters)==null?void 0:A.docs)==null?void 0:B.source}}};var C,D,G;c.parameters={...c.parameters,docs:{...(C=c.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: '日付入力テキスト'
+      }
+    }
+  },
+  args: {
+    type: 'date'
+  }
+}`,...(G=(D=c.parameters)==null?void 0:D.docs)==null?void 0:G.source}}};var H,I,J;p.parameters={...p.parameters,docs:{...(H=p.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: '日時入力テキスト'
+      }
+    }
+  },
+  args: {
+    type: 'datetime-local'
+  }
+}`,...(J=(I=p.parameters)==null?void 0:I.docs)==null?void 0:J.source}}};var K,N,Q;d.parameters={...d.parameters,docs:{...(K=d.parameters)==null?void 0:K.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: 'メールアドレス入力テキスト'
+      }
+    }
+  },
+  args: {
+    type: 'email'
+  }
+}`,...(Q=(N=d.parameters)==null?void 0:N.docs)==null?void 0:Q.source}}};var W,X,Y;i.parameters={...i.parameters,docs:{...(W=i.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: 'ファイル入力テキスト'
+      }
+    }
+  },
+  args: {
+    type: 'file'
+  }
+}`,...(Y=(X=i.parameters)==null?void 0:X.docs)==null?void 0:Y.source}}};var Z,$,ee;m.parameters={...m.parameters,docs:{...(Z=m.parameters)==null?void 0:Z.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: '非表示テキスト'
+      }
+    }
+  },
+  args: {
+    type: 'hidden',
+    label: ''
+  }
+}`,...(ee=($=m.parameters)==null?void 0:$.docs)==null?void 0:ee.source}}};var re,se,ae;u.parameters={...u.parameters,docs:{...(re=u.parameters)==null?void 0:re.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: 'image'
+      }
+    }
+  },
+  args: {
+    type: 'image'
+  }
+}`,...(ae=(se=u.parameters)==null?void 0:se.docs)==null?void 0:ae.source}}};var te,ne,oe;y.parameters={...y.parameters,docs:{...(te=y.parameters)==null?void 0:te.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: '月入力テキスト'
+      }
+    }
+  },
+  args: {
+    type: 'month'
+  }
+}`,...(oe=(ne=y.parameters)==null?void 0:ne.docs)==null?void 0:oe.source}}};var ce,pe,de;l.parameters={...l.parameters,docs:{...(ce=l.parameters)==null?void 0:ce.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: 'range'
+      }
+    }
+  },
+  args: {
+    type: 'range'
+  }
+}`,...(de=(pe=l.parameters)==null?void 0:pe.docs)==null?void 0:de.source}}};var ie,me,ue;g.parameters={...g.parameters,docs:{...(ie=g.parameters)==null?void 0:ie.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: '電話番号入力テキスト'
+      }
+    }
+  },
+  args: {
+    type: 'tel'
+  }
+}`,...(ue=(me=g.parameters)==null?void 0:me.docs)==null?void 0:ue.source}}};var ye,le,ge;T.parameters={...T.parameters,docs:{...(ye=T.parameters)==null?void 0:ye.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: '時間入力テキスト'
+      }
+    }
+  },
+  args: {
+    type: 'time'
+  }
+}`,...(ge=(le=T.parameters)==null?void 0:le.docs)==null?void 0:ge.source}}};var Te,xe,Se;x.parameters={...x.parameters,docs:{...(Te=x.parameters)==null?void 0:Te.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: 'URL入力テキスト'
+      }
+    }
+  },
+  args: {
+    type: 'url'
+  }
+}`,...(Se=(xe=x.parameters)==null?void 0:xe.docs)==null?void 0:Se.source}}};var he,fe,ke;S.parameters={...S.parameters,docs:{...(he=S.parameters)==null?void 0:he.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: '週入力テキスト'
+      }
+    }
+  },
+  args: {
+    type: 'week'
+  }
+}`,...(ke=(fe=S.parameters)==null?void 0:fe.docs)==null?void 0:ke.source}}};const Oe=["normalText","requiredText","readonlyText","passwordText","numberText","searchFieldText","colorPicker","datePicker","dateTimePicker","eMailText","fileText","hiddenText","imageText","monthText","rangeText","telText","timeText","urlText","weekText"];export{Oe as __namedExportsOrder,o as colorPicker,c as datePicker,p as dateTimePicker,Fe as default,d as eMailText,i as fileText,m as hiddenText,u as imageText,y as monthText,e as normalText,t as numberText,a as passwordText,l as rangeText,s as readonlyText,r as requiredText,n as searchFieldText,g as telText,T as timeText,x as urlText,S as weekText};
