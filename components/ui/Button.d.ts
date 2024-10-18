e54b7ed2a7b55ee32bc3b6bc4dfb939b4b89d3b5
ubata-mamezou <masato-ubata@mamezou.com>
2024-10-18 09:54:54 +0000

    Deploying to gh-pages from @ ubata-mamezou/consider-ui@c1632c58483b44aa57ceb47a5ce6b5df5238e15d 🚀

--- components/ui/Button.d.ts
+++ components/ui/Button.d.ts
@@ -3,9 +3,7 @@
  */
 export type ButtonProps = {
     /** ID(default: txt+日時から生成した文字列) */
-    id?: string;
     /** Key(default: IDと同じ) */
-    key?: string;
     /** 種類 */
     type?: `primary` | `secondary` | 'other';
     /** 表示名 */
@@ -20,5 +18,5 @@ export type ButtonProps = {
  * @param param0 {@link ButtonProps}
  * @returns Button
  */
-export declare const Button: ({ id, key, type, label, disabled, ...props }: ButtonProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Button: ({ type, label, disabled, ...props }: ButtonProps) => import("react/jsx-runtime").JSX.Element;
 //# sourceMappingURL=Button.d.ts.map
--- components/ui/Text.d.ts
+++ components/ui/Text.d.ts
@@ -4,9 +4,7 @@ import { ChangeEvent } from '../../../node_modules/react';
  */
 export type TextProps = {
     /** ID(default: txt+日時から生成した文字列) */
-    id?: string;
-    /** Key(default: IDと同じ、Keyが未設定でIDが設定されている場合はIDと同じ値を設定) */
-    key?: string;
+    /** Key(default: txt+日時から生成した文字列) */
     /** 種類(default: text) */
     type?: "text" | "color" | "date" | "datetime-local" | "email" | "file" | "hidden" | "image" | "month" | "number" | "password" | "range" | "search" | "tel" | "time" | "url" | "week";
     /** 表示名 */
@@ -32,5 +30,5 @@ export type TextProps = {
  * @param param0 各種パラメーター
  * @returns テキスト
  */
-export declare const Text: ({ id, key, type, label, value, required, disabled, readOnly, defaultValue, helperText, onChange, ...props }: TextProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Text: ({ type, label, value, required, disabled, readOnly, defaultValue, helperText, onChange, ...props }: TextProps) => import("react/jsx-runtime").JSX.Element;
 //# sourceMappingURL=Text.d.ts.map
