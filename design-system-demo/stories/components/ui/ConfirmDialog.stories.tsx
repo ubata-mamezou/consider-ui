c9db08158c93933624d853f664e5929f32c3dcea
ubata-mamezou <masato-ubata@mamezou.com>
2024-10-21 14:58:42 +0900

    ダイアログのストーリー追加

---
+++ design-system-demo/stories/components/ui/ConfirmDialog.stories.tsx
@@ -0,0 +1,43 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Button, ConfirmDialog } from '../../../src/components';
+
+// metadata
+const meta = {
+  title: 'components/ui/dialog',
+  component: ConfirmDialog,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+  },
+  args: {
+    title: 'ダイアログタイトル',
+    contentText: 'ダイアログコンテンツテキスト',
+    executeKey: {id: '1', version: 0},
+    onClickExecute: () => console.log('onClick execution button'),
+    onClickCancel: () => console.log('onClick cancel button'),
+    openDialogCondition: true,
+    onCloseDialog: () => console.log('close dialog'),
+    // label: 'ボタン表示名',
+    // onClick: () => {console.log('onClick event');},
+  },
+} satisfies Meta<typeof ConfirmDialog>;
+
+// template
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// story
+export const Basic: Story = {
+  name: '基本',
+  parameters: {
+    docs: {
+      description: {
+        story: '基本',
+      },
+    },
+  },
+  args: {
+  },
+};
