b41193cb102bf557e828cdda9ac720adb2d2c026
ubata-mamezou <masato-ubata@mamezou.com>
2024-10-30 09:47:15 +0000

    Deploying to gh-pages from @ ubata-mamezou/consider-ui@8f9d5ea274aa8557e643e1a76eae9844d2cde2ee 🚀

---
+++ assets/preview-BP2XEiTm.js
@@ -0,0 +1,16 @@
+import{ae as r,an as a,ao as i,ap as c,aq as m,ar as l,as as p}from"./index-M3AVNJv7.js";import{j as t}from"./jsx-runtime-QvZ8i92b.js";import{useMDXComponents as s}from"./index-FeUjBnvO.js";import"./iframe-BupG77V3.js";import"../sb-preview/runtime.js";import"./index-uubelm5h.js";import"./index-Dei0BBcc.js";import"./index-D-8MO0q_.js";import"./inheritsLoose-Bp8pAaMb.js";import"./index-DmeKSGxc.js";import"./index-DrFu-skq.js";function n(e){const o={hr:"hr",...s(),...e.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,`
+`,t.jsx(r,{isTemplate:!0}),`
+`,t.jsx(a,{}),`
+`,t.jsx(i,{}),`
+`,`
+`,t.jsx(c,{}),`
+`,t.jsx(m,{}),`
+`,t.jsx(o.hr,{}),`
+`,`
+`,`
+`,`
+`,`
+`,`
+`,t.jsx(l,{})]})}function x(e={}){const{wrapper:o}={...s(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(n,{...e})}):n(e)}const y={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},docs:{toc:{contentsSelector:".sbdocs-content",headingSelector:"h1, h2, h3",ignoreSelector:".sbdocs-subtitle, .css-jy82ux",title:"目次",disable:!1,unsafeTocbotOptions:{orderedList:!1}},theme:p.normal,page:x}}};export{y as default};
